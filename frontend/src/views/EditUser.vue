fc70a06ca9520c6f093658a16ef02f6e81564bc4
AdamGigiewicz <adamgigiewicz05@gmail.com>
2023-10-17 22:33:07 +0200

    feat: EditUser

--- frontend/src/views/EditUser.vue
+++ frontend/src/views/EditUser.vue
@@ -1,20 +1,57 @@
+<script setup>
+    import { Form, Field } from 'vee-validate';
+    import * as Yup from 'yup';
+
+    import { useAuthStore } from '@/stores';
+    import { ref } from 'vue';
+    import VueDatePicker from '@vuepic/vue-datepicker';
+    import '@vuepic/vue-datepicker/dist/main.css'
+
+    const date = ref(new Date());
+
+    function onSubmit(values) {
+        const authStore = useAuthStore();
+        const { login, password } = values;
+
+        return authStore.create(login, password)
+    }
+</script>
+
 <template>
-    <div class="edit-user-form">
-        <h3>Edit User</h3>
-        <form @submit.prevent="updateUser">
+    <div>
+        <h2>Edit</h2>
+        <form @submit="onSubmit">
             <div class="form-group">
-                <label for="username">Username</label>
-                <input type="text" id="username" v-model="editedUser.username" required>
+                <label>Login</label>
+                <field name="login" type="text" class="form-control" />
             </div>
             <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" id="password" v-model="editedUser.password" required>
+                <label>Password</label>
+                <field name="password" type="password" class="form-control" />
             </div>
-            <button type="submit" class="btn btn-primary">Save Changes</button>
+            <div class="form-group">
+                <label>Role</label>
+                <input type="checkbox" value="" />
+            </div>
+            <div class="form-group">
+                <label>Blocked</label>
+                <input type="checkbox" value="" />
+            </div>
+            <div class="form-group">
+                <label>Criteria</label>
+                <input type="checkbox" value="" />
+            </div>
+            <div class="form-group">
+                <label>Criteria</label>
+                <input type="checkbox" value="" />
+            </div>
+            <VueDatePicker v-model="date"></VueDatePicker>
+            <div class="form-group">
+                <button class="btn btn-primary">
+                    <span class="spinner-border spinner-border-sm mr-1"></span>
+                    sign in
+                </button>
+            </div>
         </form>
     </div>
 </template>
-
-<script setup>
-</script>
-
