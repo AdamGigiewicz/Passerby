d106133f37d7608e74a2936e874e665bf1527766
mjakobsche <m.jakobsche@proton.me>
2023-10-17 21:42:59 +0200

    missing files

---
+++ frontend/src/views/AddUser.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { Form, Field } from 'vee-validate';
+import * as Yup from 'yup';
+
+import { useAuthStore } from '@/stores';
+
+const schema = Yup.object().shape({
+    login: Yup.string().required('login is required'),
+    password: Yup.string().required('password is required')
+});
+
+function onSubmit(values, { setErrors }) {
+    const authStore = useAuthStore();
+    const { login, password} = values;
+
+    return authStore.create(login, password)
+        .catch(error => setErrors({ apiError: error }));
+}
+</script>
+
+<template>
+    <div>
+        <div class="alert alert-info">
+            login: test<br />
+            password: test
+        </div>
+        <h2>SIGN IN</h2>
+        <Form @submit="onSubmit" :validation-schema="schema" v-slot="{ errors, issubmitting }">
+            <div class="form-group">
+                <label>login</label>
+                <Field name="login" type="text" class="form-control" :class="{ 'is-invalid': errors.login}" />
+                <div class="invalid-feedback">{{errors.login}}</div>
+            </div>            
+            <div class="form-group">
+                <label>password</label>
+                <Field name="password" type="password" class="form-control" :class="{ 'is-invalid': errors.password }" />
+                <div class="invalid-feedback">{{errors.password}}</div>
+            </div>            
+            <div class="form-group">
+                <button class="btn btn-primary" :disabled="isSubmitting">
+                    <span v-show="isSubmitting" class="spinner-border spinner-border-sm mr-1"></span>
+                    sign in 
+                </button>
+            </div>
+            <div v-if="errors.apiError" class="alert alert-danger mt-3 mb-0">{{errors.apiError}}</div>
+        </Form>
+    </div>
+</template>
---
+++ frontend/src/views/EditUser.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="edit-user-form">
+        <h3>Edit User</h3>
+        <form @submit.prevent="updateUser">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" v-model="editedUser.username" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" v-model="editedUser.password" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Save Changes</button>
+        </form>
+    </div>
+</template>
+
+<script setup>
+</script>
+
